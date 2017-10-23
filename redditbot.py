904482c2480270aa19e0f19377b23ac7715fb882
Josh Barbee <32991764+joshbarbee@users.noreply.github.com>
2017-10-23 10:38:01 -0400

    Update redditbot.py

--- redditbot.py
+++ redditbot.py
@@ -48,8 +48,7 @@ def run_bot(r,repliedComments):
         elif "xD" in (comment.body) and comment.id not in repliedComments and not comment.author == r.user.me() and len(comment.body)<3:
             print ("xD: "+ comment.id)
             
-            comment.reply('ello judge jury i am here to discuss a burning issue in our subreddit the banning of the word "xd" as we all know we have used the word constantly throughout the life time of the subreddit but that still does not mean it can get annoying and or repetitive we have used xd so much that the word lost its meaning it is for the betterment of the subredditf we banned the usege of the word "xd"\n\n\n\n\n\nI am a bot...beep boop | [Source code](https://github.com/joshbarbee/Reddit-bot)|[Complaints and Suggestions](https://docs.google.com/forms/d/e/1FAIpQLSc-wGMK5DKS5xuAAKfcLivc-nR92ycZXsbYp2Y0pBkfrCtrxA/viewform?usp=sf_link)| Commands=!info, !block, !apologize")
-')
+            comment.reply('ello judge jury i am here to discuss a burning issue in our subreddit the banning of the word "xd" as we all know we have used the word constantly throughout the life time of the subreddit but that still does not mean it can get annoying and or repetitive we have used xd so much that the word lost its meaning it is for the betterment of the subredditf we banned the usege of the word "xd"\n\n\n\n\n\nI am a bot...beep boop | [Source code](https://github.com/joshbarbee/Reddit-bot)|[Complaints and Suggestions](https://docs.google.com/forms/d/e/1FAIpQLSc-wGMK5DKS5xuAAKfcLivc-nR92ycZXsbYp2Y0pBkfrCtrxA/viewform?usp=sf_link)| Commands=!info, !block, !apologize")')
             #credits to previous pasta goes to u/blastgamez
             print("replied to comment[xD]: "+comment.id)
             repliedComments.append(comment.id)
